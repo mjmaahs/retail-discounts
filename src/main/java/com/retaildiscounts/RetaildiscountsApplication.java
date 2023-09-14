2da2147742722663d71635f44bbb441388f1cb90
Mohamed Ali <mjmaahs@hotmail.com>
2023-09-15 01:03:26 +0300

    Init spring boot
    Initial commit

---
+++ src/main/java/com/retaildiscounts/RetaildiscountsApplication.java
@@ -0,0 +1,13 @@
+package com.retaildiscounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetaildiscountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetaildiscountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retaildiscounts/RetaildiscountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retaildiscounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetaildiscountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
